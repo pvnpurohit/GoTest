734c151b3faddd917d4a75e7a36ded070e70b8a9
pvnpurohit <purohit2103@gmail.com>
2024-11-23 17:53:24 +0530

    syntax fix

--- control-flow/main.go
+++ control-flow/main.go
@@ -10,7 +10,7 @@ func main() {
 	fmt.Printf("z=%v \n y=%v\n", x, y)
 	if x < 42 {
 		fmt.Println("less than 42")
-	} else if x =42 {
+	} else if x = 42 {
 		fmt.Println("equal to 42")
 	} else {
 		fmt.Println("Its greater than 42")
