95af95d42b93d3c4b227457ff7d90a18955150ee
pvnpurohit <purohit2103@gmail.com>
2024-11-23 17:56:59 +0530

    variable corrected

--- control-flow/main.go
+++ control-flow/main.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	x := 40
 	y := 5
-	fmt.Printf("z=%v \n y=%v\n", x, y)
+	fmt.Printf("x=%v \n y=%v\n", x, y)
 	if x < 42 {
 		fmt.Println("less than 42")
 	} else if x == 42 {
