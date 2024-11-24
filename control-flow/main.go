e9ff469c4275655bfef7cf0945f90ab971fc1419
pvnpurohit <purohit2103@gmail.com>
2024-11-24 17:51:50 +0530

    Exercises

--- control-flow/main.go
+++ control-flow/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
+func init() {
+	fmt.Println("This is init func")
+}
 func main() {
 	x := 40
 	y := 5
-	fmt.Printf("x=%v \n y=%v\n", x, y)
+	fmt.Printf("x=%v \ny=%v\n", x, y)
 	if x < 42 {
 		fmt.Println("less than 42")
 	} else if x == 42 {
@@ -43,5 +47,49 @@ func main() {
 			break
 		}
 		y++
+	}
+
+	a := rand.Intn(250)
+	fmt.Printf("Value of a: %v\n", a)
+	if a > 0 && a < 100 {
+		fmt.Println("a is between 0 and 100")
+	} else if a > 101 && a < 200 {
+		fmt.Println("a is between 101 and 200")
+	} else if a > 201 && a < 250 {
+		fmt.Println("a is between 201 and 250")
+	}
+
+	switch {
+	case a > 0 && a < 100:
+		fmt.Println("a is between 0 and 100")
+	case a > 101 && a < 200:
+		fmt.Println("a is between 101 and 200")
+	case a > 201 && a < 250:
+		fmt.Println("a is between 201 and 250")
+	default:
+		fmt.Println("This is default for a")
+	}
+
+	list := []int{1, 2, 3, 4, 5}
+	for index, value := range list {
+		fmt.Printf("Value at index %v: %v\n", index, value)
+	}
+
+	m := map[string]int{
+		"Praveen": 42,
+		"Sanjo":   41,
+	}
+	for name, age := range m {
+		fmt.Printf("Name: %v -> Age: %v\n", name, age)
+	}
+
+	age := m["Praveen"]
+	fmt.Printf("Age of Praveen: %v\n", age)
+
+	age = m["Bond"]
+	fmt.Printf("Age of Bond: %v\n", age)
+
+	if age, ok := m["Bond"]; !ok {
+		fmt.Printf("Age of Bond is not available and hence its %v\n", age)
 	}
 }
